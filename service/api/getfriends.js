6a64a9a7ae41c2604da3e561a253ebf92279cf0d
windowsazure <windowsazure>
2016-05-07 11:43:58 +0000

    Committing update from request /api/scmvfs/service/api/getfriends.js

--- service/api/getfriends.js
+++ service/api/getfriends.js
@@ -1,9 +1,32 @@
 exports.post = function(request, response) {
-    // Use "request.service" to access features of your mobile service, e.g.:
-    //   var tables = request.service.tables;
-    //   var push = request.service.push;
-
-    response.send(statusCodes.OK, { message : 'Hello World!' });
+    
+    var user_id = request.body.user_id;
+    var friendsTable = request.service.tables.getTable('friends');
+    
+    
+    
+    friendsTable.where( { friend:{like:user_id}}).read(
+        
+        {
+            success: function(res)
+            {
+                
+                if (res.length > 0)
+                {
+                    response.send(statusCodes.OK, { messages: "Success", user: res});
+                }
+                else
+                {
+                    response.send(statusCodes.OK, { messages: "Fail", user: []});
+                }
+                
+            }
+        }
+        
+    );
+    
+    
+    
 };
 
 exports.get = function(request, response) {
