7450b2b1aff2d0bbc846a9d40a69ba995817ad3a
windowsazure <windowsazure>
2016-05-07 08:51:19 +0000

    Committing update from request /api/scmvfs/service/api/login.js

--- service/api/login.js
+++ service/api/login.js
@@ -3,9 +3,15 @@ exports.post = function(request, response) {
     var username = request.body.username;
     var password = request.body.password;
     
+    var userStable = tables.getTable("users");
+    
+    userStable.find().then(function(result){
+       
+       response.send(statusCodes.OK, { message : 'Hello World!' });
+       
+    })
     
     
-    response.send(statusCodes.OK, { message : 'Hello World!' });
 };
 
 exports.get = function(request, response) {
