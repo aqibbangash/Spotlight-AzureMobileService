361bdb5613e411a7f4e540c73f2d20e9c453657a
windowsazure <windowsazure>
2016-05-07 09:40:20 +0000

    Committing update from request /api/scmvfs/service/api/login.js

--- service/api/login.js
+++ service/api/login.js
@@ -6,6 +6,8 @@ exports.post = function(request, response) {
     
     var usersTable = request.service.tables.getTable('users');
     
+    response.send(statusCodes.OK, { message: 'User has been found.'})
+    
 //    usersTable.lookup(username, {
 //            success: function(userRecord) {
 //                
