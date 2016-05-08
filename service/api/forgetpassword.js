de912eb0112da674db7943ba3af82d0009a1748e
windowsazure <windowsazure>
2016-05-08 10:09:42 +0000

    Committing update from request /api/scmvfs/service/api/forgetpassword.js

--- service/api/forgetpassword.js
+++ service/api/forgetpassword.js
@@ -1,11 +1,15 @@
 exports.post = function(request, response) {
+    var SendGrid = require('sendgrid').SendGrid;
+    var sendgrid = new SendGrid('azure_391f99ebb5506b93a27577ef7cd597f5@azure.com', '4mxJQRASw6ysjE1');
     var userId = request.body.email;
     var usersTable = request.service.tables.getTable('users');
     usersTable.where({email:userId}).read(
         {
             success: function(result){
                 if(result.length>0){
-                    response.send(statusCodes.OK,{data:result[0].email});
+                    var to = result[0].email;
+                    var from = "";
+                    
                 }
                 
             }
@@ -16,8 +20,7 @@ exports.post = function(request, response) {
 };
 
 exports.get = function(request, response) {
-    var SendGrid = require('sendgrid').SendGrid;
-    var sendgrid = new SendGrid('azure_391f99ebb5506b93a27577ef7cd597f5@azure.com', '4mxJQRASw6ysjE1');
+    
     sendgrid.send({
             to: 'murtazhaider@sofittech.com',
             from: 'murtazshah@gmail.com',
