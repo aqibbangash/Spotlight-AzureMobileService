dfb1af78096f6892c6f12770327c76fe68f1712d
windowsazure <windowsazure>
2016-05-08 09:28:26 +0000

    Committing update from request /api/scmvfs/service/api/forgetpassword.js

--- service/api/forgetpassword.js
+++ service/api/forgetpassword.js
@@ -7,14 +7,15 @@ exports.post = function(request, response) {
 
 exports.get = function(request, response) {
     var sendgrid = require('sendgrid')(azure_391f99ebb5506b93a27577ef7cd597f5@azure.com,4mxJQRASw6ysjE1);
-    var email = new sendgrid.Email({
-    to: 'murtazhaider@sofittech.com',
-    from: 'murtazshah@gmail.com',
-    subject: 'test mail',
-    text: 'This is a sample email message.'
-    });
-    sendgrid.send(email, function(err, json){
-    if(err) { return console.error(err); }
-    response.send(statusCodes.OK, { message : json });
-    });
+    sendgrid.send({
+            to: 'murtazhaider@sofittech.com',
+            from: 'murtazshah@gnmail.com',
+            subject: 'Forget Password',
+            text: 'A new to-do was added: '
+        }, function(success, message) {
+            // If the email failed to send, log it as an error so we can investigate
+            if (!success) {
+                console.error(message);
+            }
+        });
 };
