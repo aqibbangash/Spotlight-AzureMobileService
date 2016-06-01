bffa5dffa121a25b0dfc92ba32ee50168621fc30
windowsazure <windowsazure>
2016-06-01 12:54:32 +0000

    Committing update from request /api/scmvfs/service/api/getfriends.js

--- service/api/getfriends.js
+++ service/api/getfriends.js
@@ -4,19 +4,20 @@ exports.post = function(request, response) {
     var friendsTable = request.service.tables.getTable('friends');
     
     
+    response.send(statusCodes.OK, { messages: "Success", userId: user_id});
     
-    friendsTable.where( { friend:{like:user_id} }).read(
-        
-        {
-            success: function(res)
-            {
-                
-                    response.send(statusCodes.OK, { messages: "Success"});
-                
-            }
-        }
-        
-    );
+//    friendsTable.where( { friend:{like:user_id} }).read(
+//        
+//        {
+//            success: function(res)
+//            {
+//                
+//                    response.send(statusCodes.OK, { messages: "Success"});
+//                
+//            }
+//        }
+//        
+//    );
     
     
     
