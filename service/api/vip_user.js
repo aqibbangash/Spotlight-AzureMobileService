b1c0855a6a101f63e5509001ea2b00ec9e3ba1c9
windowsazure <windowsazure>
2016-07-26 11:55:58 +0000

    Committing update from request /api/scmvfs/service/api/vip_user.js

--- service/api/vip_user.js
+++ service/api/vip_user.js
@@ -1,25 +1,37 @@
 exports.post = function(request, response) {
-    // Use "request.service" to access features of your mobile service, e.g.:
-    //   var tables = request.service.tables;
-    //   var push = request.service.push;
+    // Requset body values 
+    var userId          = request.body.user_id;
+    // Tables
+    var userTable    = request.service.tables.getTable('Users');  
+    // Local variables
+    var result    = "";
 
-    response.send(statusCodes.OK, { message : 'Hello World!' });
-};
-function update(item, user, request) {
-    var permissionsTable = tables.getTable('permissions');
-
-    permissionsTable.where({
-        userId: user.userId,
-        permission: 'submit order'
-    }).read({
-        success: function(results) {
-            if (results.length > 0) {
-                // Permission record was found. Continue normal execution.
-                request.execute();
-            } else {
-                console.log('User %s attempted to submit an order without permissions.', user.userId);
-                request.respond(statusCodes.FORBIDDEN, 'You do not have permission to submit orders.');
+    function update(item, user, request) {
+    
+        userTable.where({
+            userId: user.userId,
+            permission: 'submit order'
+        }).read({
+            success: function(results) {
+                if (results.length > 0) {
+                    // Permission record was found. Continue normal execution.
+                    request.execute();
+                } else {
+                    console.log('User %s attempted to submit an order without permissions.', user.userId);
+                    request.respond(statusCodes.FORBIDDEN, 'You do not have permission to submit orders.');
+                }
             }
-        }
-    });
-}
+        });
+    }
+     
+     blockTable.where({both:userId}).read({
+        success : function(res){
+            var flag = (res.both).indexOf(res.id);
+            if(flag!=-1 && flag){
+                result+=res.both;    // add of id present in object
+            }
+        }// Function success end
+    });// Block table query end 
+    response.send(statusCodes.OK, { result : result});
+};
+
