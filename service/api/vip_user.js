0bb92e752a71d1e1b6f72a029f48f0fed21630f8
windowsazure <windowsazure>
2016-07-27 05:31:26 +0000

    Committing update from request /api/scmvfs/service/api/vip_user.js

--- service/api/vip_user.js
+++ service/api/vip_user.js
@@ -8,7 +8,7 @@ exports.post = function(request, response) {
             id: userId
         }).read({
             success: function(results) {
-               response.send(statusCodes.OK, { result : results});
+               response.send(statusCodes.OK, { result : results,id:userId});
                // if (results.length > 0) {
                    // results[0].vip=true;
                    // userTable.update(results[0],{
