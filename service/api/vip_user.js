c23537b29dd3f885538664f6cebe8860eb42d0ed
windowsazure <windowsazure>
2016-07-27 05:20:14 +0000

    Committing update from request /api/scmvfs/service/api/vip_user.js

--- service/api/vip_user.js
+++ service/api/vip_user.js
@@ -3,24 +3,13 @@ exports.post = function(request, response) {
     var userId          = request.body.user_id;
     // Tables
     var userTable    = request.service.tables.getTable('Users');  
-    // Local variables
-    var result    = "";
 
         userTable.where({
             userId: userId
         }).read({
             success: function(results) {
-                if (results.length > 0) {
-                    results[0].vip=1;
-                    userTable.update(results[0],{
-                       success:function(res){
-                            response.send(statusCodes.OK, { result : res});
-                       } 
-                    });
-                } else {
-                    console.log('User %s attempted to submit an order without permissions.', user.userId);
-                    request.respond(statusCodes.FORBIDDEN, 'You do not have permission to submit orders.');
-                }
+                            response.send(statusCodes.OK, { result : results});
+                
             }
         });
 };
