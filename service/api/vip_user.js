37a4f2ce10276eed94d5a513b8e407c27dcb5219
windowsazure <windowsazure>
2016-07-27 05:43:18 +0000

    Committing update from request /api/scmvfs/service/api/vip_user.js

--- service/api/vip_user.js
+++ service/api/vip_user.js
@@ -1,25 +1,23 @@
 exports.post = function(request, response) {
-    // Requset body values 
-    var userId          = request.body.user_id;
     // Tables
     var userTable    = request.service.tables.getTable('Users');  
 
         userTable.where({
-            id: 14073
+            id: request.body.user_id
         }).read({
             success: function(results) {
-               response.send(statusCodes.OK, { result : results,id:userId,requestid:request.body});
-               // if (results.length > 0) {
-                   // results[0].vip=true;
-                   // userTable.update(results[0],{
-                      // success:function(res){
-                    //        response.send(statusCodes.OK, { result : res});
-                  //     } 
-                //    });
-              //  } else {
-              //      response.send(statusCodes.OK, { result : "No such user exists."});
-              //  }
-            }
+                        if (results.length > 0) {
+                             results[0].vip=false;
+                             userTable.update(results[0],{
+                                         success:function(res){
+                                                    response.send(statusCodes.OK, { result : res});
+                                                } 
+                            });
+                        } 
+                        else {
+                              response.send(statusCodes.OK, { result : "No such user exists."});
+                        }
+                }
         });
 };
 
