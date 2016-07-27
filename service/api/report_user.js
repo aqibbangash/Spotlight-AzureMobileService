e36575d9e43b102d2c125eb17f8989cd310420dd
windowsazure <windowsazure>
2016-07-27 10:47:08 +0000

    Committing update from request /api/scmvfs/service/api/report_user.js

--- service/api/report_user.js
+++ service/api/report_user.js
@@ -1,11 +1,52 @@
 exports.post = function(request, response) {
-    // Use "request.service" to access features of your mobile service, e.g.:
-    //   var tables = request.service.tables;
-    //   var push = request.service.push;
-
-    response.send(statusCodes.OK, { message : 'Hello World!' });
+    // Tables
+    var userTable   = request.service.tables.getTable('Users');  
+    var reportTable = request.service.tables.getTable('Reports');  
+    
+    
+    reportTable.where({reporter:request.body.reporter,culprit:request.body.user_id}).read({
+        success: function(results){
+            if(results.length>0){
+                results.foreach(function(result){
+                    userTable.where({id:request.body.user_id}).read({
+                        success:function(res){
+                            res.points+=request.body.points
+                            userTable.update(res,{
+                                success: function(r){
+                                    //report insert
+                                }
+                            });
+                        }
+                    });
+                });
+            }
+            else {
+                response.send(statusCodes.OK, { result : "No such user exists."});
+            }
+        }
+    });
+    
+    userTable.where({
+        id       : request.body.user_id
+    }).read({
+        success: function(results) {
+                    if (results.length > 0) {
+                        results[0].points++;
+                         userTable.update(results[0],{
+                                        success:function(res){
+                                            response.send(statusCodes.OK, { result : res});
+                                            //reportTable.insert(res,{
+                                             //  succeess:function(r){
+                                             //      response.send(statusCodes.OK, { result : r});
+                                            //   } 
+                                           // });
+                                        } 
+                          });
+                        } 
+                        else {
+                              response.send(statusCodes.OK, { result : "No such user exists."});
+                        }
+                }
+    });
 };
 
-exports.get = function(request, response) {
-    response.send(statusCodes.OK, { message : 'Hello World!' });
-};
