e78ec3b2a43e7a8001da76f1259b4ec84885b530
windowsazure <windowsazure>
2016-07-28 05:15:34 +0000

    Committing update from request /api/scmvfs/service/api/clean_rooms.js

--- service/api/clean_rooms.js
+++ service/api/clean_rooms.js
@@ -2,27 +2,26 @@ exports.post = function(request, response) {
     // Tables
     var chatRoomTable    = request.service.tables.getTable('ChatRoom');  
     // Local variable
-    var currentDateTime  = new Date();
-        chatRoomTable.where(
-            function(dt){
-            return(this.__createdAt < dt)
-        },currentDateTime
-        ).read({
-            success: function(results) {
-                        if (results.length > 0) {
-                            results.foreach(function(result){
-                                result.__deleted=true;
-                                chatRoomTable.update(result,{
-                                    success:function(res){
-                                        response.send(statusCodes.OK, { message : "A room cleared."});
-                                    }
-                                });
-                            });
-                        } 
-                        else {
-                              response.send(statusCodes.OK, { message : "There are no rooms present."});
+    var currentDate  = new Date();
+    
+    // Get all record with date less then currentDate
+    chatRoomTable.where(function(dt){return(this.__createdAt < dt)},currentDate).read({
+        // Set __delete column to true
+        success: function(results) {
+            if (results.length > 0) {
+                results.foreach(function(result){
+                    result.__deleted=true;
+                    chatRoomTable.update(result,{
+                        success:function(res){
+                            response.send(statusCodes.OK, { message : "A room cleared."});
                         }
-                }
-        });
+                    });
+                });
+            } 
+            else {
+                  response.send(statusCodes.OK, { message : "There are no rooms present."});
+            }
+        }
+    });
 };
 
