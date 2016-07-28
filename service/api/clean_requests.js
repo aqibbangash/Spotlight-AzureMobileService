2d1f3846e70cc060359233a8ec37c4715bea3998
windowsazure <windowsazure>
2016-07-28 05:12:27 +0000

    Committing update from request /api/scmvfs/service/api/clean_requests.js

--- service/api/clean_requests.js
+++ service/api/clean_requests.js
@@ -2,27 +2,26 @@ exports.post = function(request, response) {
     // Tables
     var requestTable    = request.service.tables.getTable('Request');  
     // Local variable
-    var currentDateTime  = new Date();
-        requestTable.where(
-            function(dt){
-            return(this.__createdAt < dt)
-        },currentDateTime
-        ).read({
-            success: function(results) {
-                        if (results.length > 0) {
-                            results.foreach(function(result){
-                                result.__deleted=true;
-                                requestTable.update(result,{
-                                    success:function(res){
-                                        response.send(statusCodes.OK, { message : "A chat cleared."});
-                                    }
-                                });
-                            });
-                        } 
-                        else {
-                              response.send(statusCodes.OK, { message : "There are no chats present."});
+    var currentDate  = new Date();
+    
+    // get all record before currentDate
+    requestTable.where(function(dt){return(this.__createdAt < dt)},currentDate).read({
+        // Set __delete column to false
+        success: function(results) {
+            if (results.length > 0) {
+                results.foreach(function(result){
+                    result.__deleted=true;
+                    requestTable.update(result,{
+                        success:function(res){
+                            response.send(statusCodes.OK, { message : "A chat cleared."});
                         }
-                }
-        });
+                    });
+                });
+            } 
+            else {
+                response.send(statusCodes.OK, { message : "There are no chats present."});
+            }
+        }
+    });
 };
 
