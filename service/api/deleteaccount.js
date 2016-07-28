1b8ad91b1b121d70d25d9349ad47e42d29012484
windowsazure <windowsazure>
2016-07-28 05:27:10 +0000

    Committing update from request /api/scmvfs/service/api/deleteaccount.js

--- service/api/deleteaccount.js
+++ service/api/deleteaccount.js
@@ -1,40 +1,44 @@
 exports.post = function(request, response) {
+    // Request body values
+    var userId  = request.body.user_id;
     // Tables
     var friendsTable    = request.service.tables.getTable('friends');  
     var usersTable      = request.service.tables.getTable('Users');  
 
-    // Delete friends
-    friendsTable.where({friend:request.body.user_id}).read({
+    // Get all records with friend equals to userId
+    friendsTable.where({friend:userId}).read({
+        // Delete friends
         success: function(results) {
-                    if (results.length > 0) {
-                        results.foreach(function(result){
-                            friendsTable.del(result,{
-                                    success:function(res){
-                                                response.send(statusCodes.OK, { message : "Friend deleted."});
-                                     } 
-                            });         
-                        });
-                    } 
-                    else {
-                      response.send(statusCodes.OK, { message : "No user found"});
-                    }
-                }        
+            if (results.length > 0) {
+                results.foreach(function(result){
+                    friendsTable.del(result,{
+                        success:function(res){
+                            response.send(statusCodes.OK, { message : "Friend deleted."});
+                         } 
+                    });         
+                });
+            } 
+            else {
+                response.send(statusCodes.OK, { message : "No user found"});
+            }
+        }        
     });
     
-    // Delete user
+    // Get record with id equals userId
     usersTable.where({id:request.body.user_id}).read({
-            success: function(results) {
-                        if (results.length > 0) {
-                             usersTable.del(results[0],{
-                                         success:function(res){
-                                                    response.send(statusCodes.OK, { message : "User deleted."});
-                                                } 
-                            });
-                        } 
-                        else {
-                              response.send(statusCodes.OK, { message : "No user found"});
-                        }
-                }        
+        // Delete user
+        success: function(results) {
+            if (results.length > 0) {
+                usersTable.del(results[0],{
+                    success:function(res){
+                        response.send(statusCodes.OK, { message : "User deleted.",completed:true});
+                    } 
+                });
+            } 
+            else {
+                response.send(statusCodes.OK, { message : "No user found"});
+            }
+        }        
     });
 };
 
