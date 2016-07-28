3d60e41b45b93751e6ff8edc8bd07a4dfe97aecf
windowsazure <windowsazure>
2016-07-28 05:53:27 +0000

    Committing update from request /api/scmvfs/service/api/point_plus_plus.js

--- service/api/point_plus_plus.js
+++ service/api/point_plus_plus.js
@@ -1,29 +1,47 @@
 exports.post = function(request, response) {
+    // Request body values
+    var userId      = request.body.user_id;
+    var reporter    = "";
     // Tables
-    var userTable    = request.service.tables.getTable('Users');  
-    var reportTable    = request.service.tables.getTable('Reports');  
-    
-    userTable.where({
-        id       : request.body.user_id
-    }).read({
+    var userTable   = request.service.tables.getTable('Users');  
+    var reportTable = request.service.tables.getTable('Reports');  
+    // Get all records with id equals userId 
+    userTable.where({id : userId}).read({
+        // Set point column to plus one
         success: function(results) {
-                    if (results.length > 0) {
-                        results[0].points++;
-                         userTable.update(results[0],{
-                                        success:function(res){
-                                            response.send(statusCodes.OK, { result : res});
-                                            //reportTable.insert(res,{
-                                             //  succeess:function(r){
-                                             //      response.send(statusCodes.OK, { result : r});
-                                            //   } 
-                                           // });
-                                        } 
-                          });
-                        } 
-                        else {
-                              response.send(statusCodes.OK, { result : "No such user exists."});
+            if (results.length > 0) {
+                results[0].points++;
+                userTable.update(results[0],{
+                    // Insert new record in reports
+                    success:function(res){
+                        //response.send(statusCodes.OK, { result : res});
+             
+                        function insert(item, user, request) {
+                            reportTable.where({ reporter : reporter,culprit : userId }).read({
+                                success: function (items) {
+                                    if (items.length > 0) {
+                                        request.respond(400, { error: 'Item with this name already exists' });
+                                    } 
+                                    else {
+                                        request.execute();
+                                        response.send(statusCodes.OK, { result : item});
+                                    }
+                                }
+                            });
                         }
-                }
+                        
+                        //reportTable.insert(res,{
+                        //    succeess:function(r){
+                        //        response.send(statusCodes.OK, { result : r});
+                        //    } 
+                        //});
+                    } 
+              });
+            } 
+            else {
+                  response.send(statusCodes.OK, { result : "No such user exists."});
+            }
+        }
     });
 };
 
