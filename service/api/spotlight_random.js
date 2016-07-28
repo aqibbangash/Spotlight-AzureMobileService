1e47cadd71acc3cad45e987964ae475ded258fa2
windowsazure <windowsazure>
2016-07-28 10:42:29 +0000

    Committing update from request /api/scmvfs/service/api/spotlight_random.js

--- service/api/spotlight_random.js
+++ service/api/spotlight_random.js
@@ -1,11 +1,6 @@
 exports.post = function(request, response) {
-    // Use "request.service" to access features of your mobile service, e.g.:
-    //   var tables = request.service.tables;
-    //   var push = request.service.push;
-
-    response.send(statusCodes.OK, { message : 'Hello World!' });
-};
-
-exports.get = function(request, response) {
+    // Request body values
+    
+    // t  
     response.send(statusCodes.OK, { message : 'Hello World!' });
 };
