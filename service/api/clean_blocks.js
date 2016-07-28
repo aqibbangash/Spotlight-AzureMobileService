2b5527cb466cb4bfa3308d42e0f27dd8c2d01f61
windowsazure <windowsazure>
2016-07-28 05:07:21 +0000

    Committing update from request /api/scmvfs/service/api/clean_blocks.js

--- service/api/clean_blocks.js
+++ service/api/clean_blocks.js
@@ -1,24 +1,23 @@
-exports.post = function(request, response) {
+exports.post = function(request, response) {     
     // Tables
     var blockTable    = request.service.tables.getTable('Block');  
-
-        blockTable.where({
-            blocktype       : '1'
-        }).read({
-            success: function(results) {
-                        if (results.length > 0) {
-                            results.foreach(function(result){
-                                blockTable.del(result,{
-                                    success:function(res){
-                                        response.send(statusCodes.OK, { message : "All blocks clear."});
-                                    }
-                                });
-                            });
-                        } 
-                        else {
-                              response.send(statusCodes.OK, { message : "There are no blocks."});
+    // get all records with blocktype 1
+    blockTable.where({blocktype : '1'}).read({
+        // delete all record in blockTable
+        success: function(results) {
+            if (results.length > 0) {
+                results.foreach(function(result){
+                    blockTable.del(result,{
+                        success:function(res){
+                            response.send(statusCodes.OK, { message : "All blocks clear."});
                         }
-                }
-        });
+                    });
+                });
+            } 
+            else {
+                  response.send(statusCodes.OK, { message : "There are no blocks."});
+            }
+        }
+    });
 };
 
