89f5975f57d117be0e63613555729f9778a48650
windowsazure <windowsazure>
2016-07-28 11:17:25 +0000

    Committing update from request /api/scmvfs/service/api/spotlight_random.js

--- service/api/spotlight_random.js
+++ service/api/spotlight_random.js
@@ -10,10 +10,10 @@ exports.post = function(request, response) {
     var requestTable    = request.service.tables.getTable('Request');  
     var blockTable    = request.service.tables.getTable('Block');  
     var userTable    = request.service.tables.getTable('Users');  
-    
+    // Get all records with __deleted values false and id equals userId
     requestTable.where({__deleted : false, id : userId}).read({
        success : function(_requests){
-       response.send(statusCodes.OK, { message : _requests });    
+       //response.send(statusCodes.OK, { message : _requests });    
        } 
     });
 };
