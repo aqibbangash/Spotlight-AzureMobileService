980112cbf4ae2caa13c6e9186f19c02df4925a55
windowsazure <windowsazure>
2016-07-28 06:25:17 +0000

    Committing update from request /api/scmvfs/service/api/report_user.js

--- service/api/report_user.js
+++ service/api/report_user.js
@@ -1,23 +1,26 @@
 exports.post = function(request, response) {
+    // Request body values
+    var userId      = request.body.user_id;
+    var points      = request.body.points;
+    var reporter    = request.body.reporter;
     // Tables
     var userTable   = request.service.tables.getTable('Users');  
     var reportTable = request.service.tables.getTable('Reports');  
     
-    
+    // Get all records with reporter equals reporter and culprit equals userId
     reportTable.where({reporter:request.body.reporter,culprit:request.body.user_id}).read({
+        // 
         success: function(results){
-            if(results.length>0){
-                results.foreach(function(result){
-                    userTable.where({id:request.body.user_id}).read({
-                        success:function(res){
-                            res.points+=request.body.points
-                            userTable.update(res,{
-                                success: function(r){
-                                    //report insert
-                                }
-                            });
-                        }
-                    });
+            if(results.length == 0){
+                userTable.where({id : userId}).read({
+                    success:function(res){
+                        res.points += points;
+                        userTable.update(res,{
+                            success:function(r){
+                                //response.send(statusCodes.OK, { result : "User point update."});                
+                            }
+                        })
+                    }
                 });
             }
             else {
