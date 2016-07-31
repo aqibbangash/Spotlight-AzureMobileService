2756e0f3fc04dae6a14765977a832c31bf1163e4
windowsazure <windowsazure>
2016-07-31 02:33:02 +0000

    Committing update from request /api/scmvfs/service/api/test_insert.js

--- service/api/test_insert.js
+++ service/api/test_insert.js
@@ -2,7 +2,23 @@ exports.post = function(request, response) {
     // Tables
     var userTable    = request.service.tables.getTable('Users');  
     
-    
-
-    response.send(statusCodes.OK, { message : 'Hello World!' });
+    userTable.insert({
+        first_name:"test",
+        last_name:"test1",
+        gender:"a",
+        country:"Pakistan",
+        city:"Islamabad",
+        age:"34",
+        quickblox_id:"",
+        profile_pic:"",
+        email:"test@yahoo.com",
+        password:"12345",
+        points:"100",
+        prefs:"M",
+        vip:false
+    },{
+        success: function(obj){
+            response.send(statusCodes.OK, { message : obj });
+        }
+    });    
 };
