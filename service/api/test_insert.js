2b16a144c3e3b66825a8dc5611a2cb3b968003c2
windowsazure <windowsazure>
2016-08-01 04:31:14 +0000

    Committing update from request /api/scmvfs/service/api/test_insert.js

--- service/api/test_insert.js
+++ service/api/test_insert.js
@@ -1,24 +1,33 @@
 exports.post = function(request, response) {
+    // Insert
     // Tables
-    var userTable    = request.service.tables.getTable('Users');  
+   // var userTable    = request.service.tables.getTable('Users');  
+   // userTable.insert({
+   //     first_name:"test",
+   //     last_name:"test1",
+   //    gender:"a",
+   //     country:"Pakistan",
+   //     city:"Islamabad",
+   //     age:"34",
+   //     quickblox_id:"",
+   //     profile_pic:"",
+   //     email:"test@yahoo.com",
+   //     password:"12345",
+   //     points:"100",
+   //     prefs:"M",
+   //     vip:false
+   // },{
+   //     success: function(obj){
+   //         response.send(statusCodes.OK, { message : obj });
+   //     }
+   // });    
     
-    userTable.insert({
-        first_name:"test",
-        last_name:"test1",
-        gender:"a",
-        country:"Pakistan",
-        city:"Islamabad",
-        age:"34",
-        quickblox_id:"",
-        profile_pic:"",
-        email:"test@yahoo.com",
-        password:"12345",
-        points:"100",
-        prefs:"M",
-        vip:false
-    },{
-        success: function(obj){
-            response.send(statusCodes.OK, { message : obj });
-        }
-    });    
+    // Update
+    var requestTable    = request.service.tables.getTable('Request');
+    
+    requestTable.update({user_id:'13399'},{
+      success:function(res){
+          response.send(statusCodes.OK,{message:res})
+      }  
+    });
 };
