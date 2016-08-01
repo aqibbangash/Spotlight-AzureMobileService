1970812ee0f2cfc82d1731b7733d384b50a58589
windowsazure <windowsazure>
2016-08-01 07:41:14 +0000

    Committing update from request /api/scmvfs/service/api/spotlight_text.js

--- service/api/spotlight_text.js
+++ service/api/spotlight_text.js
@@ -8,8 +8,48 @@ exports.post = function(request, response) {
     var numAlready      = 0;
     var connectedWith   = "";
     var countIDK        = 0;
-    var check           = false;         
+    var check           = false;     
+    var requestId       = "";    
     // Tables
+    var userTable    = request.service.tables.getTable('Users');
+    var requestTable    = request.service.tables.getTable('Request');
     
-    response.send(statusCodes.OK, { message : 'Hello World!' });
+    // Get all requests of type text
+    requestTable.where({type : 'text',user_id : userId}).read({
+        success : function(requests){
+            requests.forEach(function(request){
+                numAlready++;
+                requestId = request.id;
+                if(request.completed){
+                    connectedWith = request.other_user;
+                }
+            });
+            if(numAlready > 0){
+                if(connectedWith != null){
+                    userTable.where({id : connectedWith}).read({
+                        success : function(user){
+                         response.send(statusCodes.OK, { 
+                             boolean        : true,
+                             requestId      : requestId,
+                             type           : 'Exist and match first try',
+                             id             : user.id,
+                             full_name      : user.first_name+" "+user.last_name,
+                             gender         : user.gender,
+                             city           : user.city,
+                             country        : user.country,
+                             age            : user.age,
+                             profile_pic    : user.profile_pic, 
+                             vip            : user.vip
+                             });   
+                        }
+                    });
+                }
+                else {
+                    blockTable.where()    
+                }    
+            }
+        }    
+    });
+    
+    //response.send(statusCodes.OK, { message : 'Hello World!' });
 };
