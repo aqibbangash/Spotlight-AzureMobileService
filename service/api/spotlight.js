0f3b3986a23ab79b48ad5f24ab3368d33b1ecba1
windowsazure <windowsazure>
2016-08-02 04:55:43 +0000

    Committing update from request /api/scmvfs/service/api/spotlight.js

--- service/api/spotlight.js
+++ service/api/spotlight.js
@@ -44,7 +44,32 @@ exports.post = function(request, response) {
                                             
                                             users.forEach(function(user){
                                                 if(prefs.indexOf(user.gender) !== -1 && user.prefs.indexOf(userGender) !== -1){
-                                                    
+                                                    requestTable.where({user_id : user_id, completed : 0, other_user : null}).read({
+                                                        success : function(requests){
+                                                            requests[0].completed = 1;
+                                                            requests[0].other_user = user.id;
+                                                            requestTable.update(requests[0],{
+                                                                success : function(res){
+                                                                    if(res.count > 0){
+                                                                        requestTable.where({user_id : user.id, completed : 0, other_user : null}).read({
+                                                                            success : function(requests){
+                                                                                requests[0].completed = 1;
+                                                                                requests[0].other_user = user.id;
+                                                                                requestTable.update(requests[0],{
+                                                                                    success : function(){
+                                                                                        response.send(statusCodes.OK, { boolean : true , match_id : user.id });   
+                                                                                    }
+                                                                                });
+                                                                            }
+                                                                        });
+                                                                    }
+                                                                    else {
+                                                                        
+                                                                    }
+                                                                }
+                                                            }); 
+                                                        }
+                                                    });           
                                                 }
                                             });
                                         }
