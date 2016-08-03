605937a672c91b7f7bb36e47aca167e95d6688a0
windowsazure <windowsazure>
2016-08-03 04:48:13 +0000

    Committing update from request /api/scmvfs/service/api/spotlight_video.js

--- service/api/spotlight_video.js
+++ service/api/spotlight_video.js
@@ -9,7 +9,7 @@ exports.post = function(request, response) {
     var connectedWith   = "";
     var countIDK        = 0;
     var check           = false;     
-    //var requestId       = "";
+    var requestId       = "";
     //var temp            = [];
     //var onlineUsers     = [];    
     // Tables
@@ -23,6 +23,7 @@ exports.post = function(request, response) {
             if(requests.length > 0){
                 requests.forEach(function(request){
                     numAlready++;
+                    requestId = request.id;
                     if(request.completed){
                         connectedWith = request.other_user;
                     }
@@ -38,11 +39,27 @@ exports.post = function(request, response) {
                     userTable.where({id : connectedWith}).read({
                         success : function(user){
                             if(user){
-                                response.send(statusCodes.OK, { 
-                                    boolean : true 
-                                    });    
+                         response.send(statusCodes.OK, { 
+                             boolean        : true,
+                             requestId      : requestId,
+                             type           : 'Exist and match first try',
+                             id             : user.id,
+                             full_name      : user.first_name+" "+user.last_name,
+                             gender         : user.gender,
+                             city           : user.city,
+                             country        : user.country,
+                             age            : user.age,
+                             profile_pic    : user.profile_pic, 
+                             vip            : user.vip
+                             });    
                             }
                         }
+                    });
+                }
+                else {
+                    // Get all blocked users
+                    blockTable.where(function(u){return this.user_id.indexOf(user_id) !== -1;},user_id).read({
+                        success : function()
                     });
                 }
             }
