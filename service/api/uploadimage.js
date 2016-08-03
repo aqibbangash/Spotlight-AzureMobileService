48f51d528d3affdb16e3e2c5e60390046338fa1c
windowsazure <windowsazure>
2016-08-03 09:40:52 +0000

    Committing update from request /api/scmvfs/service/api/uploadimage.js

--- service/api/uploadimage.js
+++ service/api/uploadimage.js
@@ -3,22 +3,8 @@ exports.post = function(request, response) {
     var name = request.body.name;
     // 3rd party extension
     //var cloudinary = require('cloudinary')();
-  var SendGrid = require('sendgrid').SendGrid;
-    var Cloudinary = require('cloudinary')();
-    
-    var cloudinary = new Cloudinary();
-    
-    //cloudinary.config({});
-        var sendgrid = new SendGrid('azure_391f99ebb5506b93a27577ef7cd597f5@azure.com', '4mxJQRASw6ysjE1');
+  var Cloudinary = require('cloudinary').Cloudinary;
 
-     response.send(statusCodes.OK, { message : 'ello World!', name : cloudinary});
-            
-    //cloudinary.config({cloud_name : 'hdemxqeq3', api_key : '636525738353489', api_secret : 'vBhen3A7vcAQ7QF6PNvUrsS4kCk' });
-    
-    
-    //cloudinary.uploader.upload('http://image.shutterstock.com/z/stock-vector-grunge-rubber-stamp-with-word-lame-vector-illustration-151984781.jpg',function(result) { 
-        //response.send(statusCodes.OK, { message : 'Hello World!',result : result});});
-    //Upload
-    response.send(statusCodes.OK, { message : 'Hello World!', name : SendGrid});
+response.send(statusCodes.OK, { message : 'Hello World!', name : Cloudinary});
     
 };
