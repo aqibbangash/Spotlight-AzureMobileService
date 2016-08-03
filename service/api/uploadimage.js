a0afbb7c9c358ffc487990a64a10edfa6dd32fe2
windowsazure <windowsazure>
2016-08-03 11:08:18 +0000

    Committing update from request /api/scmvfs/service/api/uploadimage.js

--- service/api/uploadimage.js
+++ service/api/uploadimage.js
@@ -2,7 +2,7 @@ exports.post = function(request, response) {
     // Request body values
     //var name = request.body.name;
     // 3rd party extension
-   var cloudinary = new Cloudinary();
+   var cloudinary = require('cloudinary://345146331124229:UPjachvXs18IxVOJfVoAn2sTw_M@hezfpayoy');
 //cloudinary.uploader.upload("my_picture.jpg")
 
 response.send(statusCodes.OK, { message : 'Hello World!',});
