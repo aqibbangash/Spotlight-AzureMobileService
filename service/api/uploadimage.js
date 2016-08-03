37fa406266ebcdfc4b5d3e085fe5e2d4d3e42c88
windowsazure <windowsazure>
2016-08-03 07:34:03 +0000

    Committing update from request /api/scmvfs/service/api/uploadimage.js

--- service/api/uploadimage.js
+++ service/api/uploadimage.js
@@ -8,8 +8,9 @@ exports.post = function(request, response) {
     cloudinary.config({cloud_name : 'hdemxqeq3', api_key : '636525738353489', api_secret : 'vBhen3A7vcAQ7QF6PNvUrsS4kCk' });
     
     
-    cloudinary.uploader.upload('http://image.shutterstock.com/z/stock-vector-grunge-rubber-stamp-with-word-lame-vector-illustration-151984781.jpg');
+    cloudinary.uploader.upload('http://image.shutterstock.com/z/stock-vector-grunge-rubber-stamp-with-word-lame-vector-illustration-151984781.jpg',function(result) { 
+        response.send(statusCodes.OK, { message : 'Hello World!',result : result});});
     //Upload
     
-    response.send(statusCodes.OK, { message : 'Hello World!'});
+    
 };
