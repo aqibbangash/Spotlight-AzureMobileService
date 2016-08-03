eecc608f3edcdaa9544e74abe110f09dbf7ddb76
windowsazure <windowsazure>
2016-08-03 07:46:43 +0000

    Committing update from request /api/scmvfs/service/api/uploadimage.js

--- service/api/uploadimage.js
+++ service/api/uploadimage.js
@@ -4,11 +4,14 @@ exports.post = function(request, response) {
     // 3rd party extension
     //var cloudinary = require('cloudinary')();
   var SendGrid = require('sendgrid').SendGrid;
+    var Cloudinary = require('cloudinary').Cloudinary;
     
+    var cloudinary = new Cloudinary();
     
+    //cloudinary.config({});
         var sendgrid = new SendGrid('azure_391f99ebb5506b93a27577ef7cd597f5@azure.com', '4mxJQRASw6ysjE1');
 
-     response.send(statusCodes.OK, { message : 'ello World!', name : sendgrid});
+     response.send(statusCodes.OK, { message : 'ello World!', name : cloudinary});
             
     //cloudinary.config({cloud_name : 'hdemxqeq3', api_key : '636525738353489', api_secret : 'vBhen3A7vcAQ7QF6PNvUrsS4kCk' });
     
