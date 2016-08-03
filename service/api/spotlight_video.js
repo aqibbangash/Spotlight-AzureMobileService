fab2f87a116ddf48166b6f2576ddcb20cc6397f7
windowsazure <windowsazure>
2016-08-03 04:43:55 +0000

    Committing update from request /api/scmvfs/service/api/spotlight_video.js

--- service/api/spotlight_video.js
+++ service/api/spotlight_video.js
@@ -1,7 +1,57 @@
 exports.post = function(request, response) {
-    // Use "request.service" to access features of your mobile service, e.g.:
-    //   var tables = request.service.tables;
-    //   var push = request.service.push;
-
-    response.send(statusCodes.OK, { message : 'Hello World!' });
+    // Request body values
+    var user_id      = request.body.user_id;
+    var userGender  = request.body.gender;
+    var prefs       = request.body.prefs;
+    // Local variables
+    var blockedUsers    = "";
+    var numAlready      = 0;
+    var connectedWith   = "";
+    var countIDK        = 0;
+    var check           = false;     
+    //var requestId       = "";
+    //var temp            = [];
+    //var onlineUsers     = [];    
+    // Tables
+    var userTable    = request.service.tables.getTable('Users');
+    var requestTable    = request.service.tables.getTable('Request');
+    var blockTable    = request.service.tables.getTable('Block');
+    
+    // Get all requests 
+    requestTable.where({user_id : user_id, type : 'video'}).read({
+        success : function(requests){
+            if(requests.length > 0){
+                requests.forEach(function(request){
+                    numAlready++;
+                    if(request.completed){
+                        connectedWith = request.other_user;
+                    }
+                });    
+            }
+            else {
+                // No requests present    
+            }
+            //  requests exsists
+            if(numAlready > 0){
+                // Partner exists
+                if(connectedWith != null){
+                    userTable.where({id : connectedWith}).read({
+                        success : function(user){
+                            if(user){
+                                response.send(statusCodes.OK, { 
+                                    boolean : true 
+                                    });    
+                            }
+                        }
+                    });
+                }
+            }
+            // No request exists
+            else {
+                
+            }
+        }
+    });
+    
+    
 };
