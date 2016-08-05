0b9373e1ad5df79c0557689db0c8ced6209b8fd8
windowsazure <windowsazure>
2016-08-05 06:05:26 +0000

    Committing update from request /api/scmvfs/service/api/sendemail.js

--- service/api/sendemail.js
+++ service/api/sendemail.js
@@ -1,6 +1,22 @@
 exports.post = function(request, response) {
     // Third party config
     var SendGrid = require('sendgrid').SendGrid;
+    
+    
+        var sendgrid = new SendGrid('azure_391f99ebb5506b93a27577ef7cd597f5@azure.com', '4mxJQRASw6ysjE1');
 
-    response.send(statusCodes.OK, { message : SendGrid });
+        sendgrid.send({
+            to: 'aqibbangash@sofittech.com',
+            from: 'noreply@spotlight.com',
+            subject: 'Api test Email',
+            text: 'This is the plain text version of the email content '
+        }, function(success, message) {
+            // If the email failed to send, log it as an error so we can investigate
+            if (!success) {
+                console.error(message);
+            }
+            else {
+                response.send(statusCodes.OK, { message : 'Email sent!' });
+            }
+        });
 };
