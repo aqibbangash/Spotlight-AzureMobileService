989498958ea23f1d5bebe11bac24213e15019995
windowsazure <windowsazure>
2016-08-08 06:27:59 +0000

    Committing update from request /api/scmvfs/service/api/report_user.js

--- service/api/report_user.js
+++ service/api/report_user.js
@@ -7,40 +7,31 @@ exports.post = function(request, response) {
     var userTable   = request.service.tables.getTable('Users');  
     var reportTable = request.service.tables.getTable('Reports');  
     var blockTable = request.service.tables.getTable('Block');  
+    
     // Get all records with reporter equals reporter and culprit equals userId
-    reportTable.where({reporter:request.body.reporter,culprit:request.body.user_id}).read({
+    reportTable.where({reporter : reporter, culprit : userId}).read({
         // Set point column to points+request points
         success: function(results){
-            response.send(statusCodes.OK, { result : results});
+            //response.send(statusCodes.OK, { result : results});
             if(results.length == 0){
-                //userTable.where({id : userId}).read({
-                    //success:function(res){
-                     //   res.points += points;
-                        //userTable.update(res,{
-                            // Insert record in reports table 
-                            //success:function(r){
-                                //response.send(statusCodes.OK, { result : r});   
-                                //reportTable.insert({reporter : reporter,culprit : userId},{
-                                    // Insert record in block table
-                                //    success: function (items) {
-                                    //    response.send(statusCodes.OK, { result : items,status : 1});
-                                    //    blockTable.insert({ blocker   : reporter,
-                                    //                        blocky    : userId,
-                                    //                        both      : userId+reporter,
-                                    //                        blocktype : '1'},{
-                                    //                           success:function(blockRecord){
-                                    //                               response.send(statusCodes.OK, { result : blockRecord,status : 1});
-                                    //                           } 
-                                    //                        });
-                                //    }
-                                //});             
-                            //}
-                        //});
-                    //}
-                //});
+                userTable.where({user_id : userId}).read({
+                    success : function(users){
+                        if(users.count > 0){
+                            users[0].points += 1;
+                            userTable.update(users[0],{
+                                success : function(user){
+                                    
+                                }
+                            });
+                        }
+                        else {
+                            response.send(statusCodes.OK, { result : "No such user exists.", status : 0});
+                        }
+                    }
+                });
             }
             else {
-                response.send(statusCodes.OK, { result : "No such user exists.", status : 0});
+                response.send(statusCodes.OK, { result : "No report exists.", status : 0});
             }
         }
     });
