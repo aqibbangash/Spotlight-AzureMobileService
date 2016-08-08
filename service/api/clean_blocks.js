98c45bb2884b81825bf9c5b563141b71924a8982
windowsazure <windowsazure>
2016-08-08 05:01:15 +0000

    Committing update from request /api/scmvfs/service/api/clean_blocks.js

--- service/api/clean_blocks.js
+++ service/api/clean_blocks.js
@@ -1,7 +1,8 @@
 exports.post = function(request, response) {     
     // Tables
     var blockTable    = request.service.tables.getTable('Block');  
-    // get all records with blocktype 1
+    
+    // Get all records with blocktype 1
     blockTable.where({blocktype : '1'}).read({
         // delete all record in blockTable
         success: function(results) {
