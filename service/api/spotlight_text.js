10fb1a66e10df72dcc90008e6bd20a2ba3f4c4be
windowsazure <windowsazure>
2016-08-09 05:13:46 +0000

    Committing update from request /api/scmvfs/service/api/spotlight_text.js

--- service/api/spotlight_text.js
+++ service/api/spotlight_text.js
@@ -14,12 +14,28 @@ exports.post = function(request, response) {
     var temp           = [];
     var onlineUsers    = [];    
     // Tables
-    //var userTable    = request.service.tables.getTable('Users');
-    //var requestTable = request.service.tables.getTable('Request');
-    //var blockTable   = request.service.tables.getTable('Block');
+    var userTable    = request.service.tables.getTable('Users');
+    var requestTable = request.service.tables.getTable('Request');
+    var blockTable   = request.service.tables.getTable('Block');
     
     // Get all request type text and user_id equals user_id
-    
+        requestTable.where({type : 'text', user_id : user_id}).read({
+        success : function(requests){
+         response.send(statusCodes.OK, { message : requests });  // Test log 
+            if(requests.length > 0){
+                requests.forEach(function(request){
+                    numAlready++;
+                    requestId = request.id;
+                    if(request.completed){
+                        connectedWith = request.other_user;
+                    }
+                });
+            }
+            else {
+             response.send(statusCodes.OK, { message : 'No request are present for this user.' });   
+            }
+        }    
+    });
     
     
 };
