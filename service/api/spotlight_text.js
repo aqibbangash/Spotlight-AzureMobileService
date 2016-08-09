aa28d7c4034a0d97332018583f2131a12171551b
windowsazure <windowsazure>
2016-08-09 06:22:46 +0000

    Committing update from request /api/scmvfs/service/api/spotlight_text.js

--- service/api/spotlight_text.js
+++ service/api/spotlight_text.js
@@ -88,7 +88,15 @@ exports.post = function(request, response) {
                         }
                     });
                     // Get Online users
-                  ;
+                    requestTable.where(
+                        function(u){
+                            return this.user_id != user_id && abc.toString().indexOf(this.user_id) == -1  && this.type == 'text' && this.completed == false
+                            },user_id).read({
+                        success : function(requests){
+                            response.send(statusCodes.OK, { message : requests });  // Test log 
+                            
+                        }
+                    });
                 }
             }
             else {
