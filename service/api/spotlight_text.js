3166b56243c31649ec563a5541aa4c80ef8af0ce
windowsazure <windowsazure>
2016-08-10 11:46:57 +0000

    Committing update from request /api/scmvfs/service/api/spotlight_text.js

--- service/api/spotlight_text.js
+++ service/api/spotlight_text.js
@@ -98,7 +98,7 @@ exports.post = function(request, response) {
                                 if(onlineUsers.length > 0){
                                     //var x = function(ou){console.log("test user : ",ou.indexOf('6990') != -1);return ou.indexOf('6990') != -1;}
                                     // Get online user
-                                    userTable.where(function(ou){return ou.indexOf('6990') != -1;},onlineUsers).read({
+                                    userTable.where(function(ou){return ou.indexOf('6990') == -1;},onlineUsers).read({
                                         success : function(users){
                                             response.send(statusCodes.OK, { success : users});
                                             //console.log("success 124 : ",users);
