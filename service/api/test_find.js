2e04c4af08c946118af6b3fe6125a6675813b8b9
windowsazure <windowsazure>
2016-08-11 10:03:12 +0000

    Committing update from request /api/scmvfs/service/api/test_find.js

--- service/api/test_find.js
+++ service/api/test_find.js
@@ -1,6 +1,6 @@
 exports.post = function(request, response) {
 
-    var onlineUsers    = ["6990"];    
+    var onlineUsers    = ["6990","23091"];    
     // Tables
     var userTable    = request.service.tables.getTable('Users');
         //var test = function(obj){return onlineUsers.indexOf(obj) != -1;}
