1481a642e742234d0d7913bf6a317e1c1b369136
windowsazure <windowsazure>
2016-08-11 05:50:59 +0000

    Committing update from request /api/scmvfs/service/api/test_find.js

--- service/api/test_find.js
+++ service/api/test_find.js
@@ -3,8 +3,8 @@ exports.post = function(request, response) {
     var onlineUsers    = ['6990'];    
     // Tables
     var userTable    = request.service.tables.getTable('Users');
-        var test = function(obj){return onlineUsers.indexOf(obj) != -1;}
-        userTable.where(function(){return test(this.id.toString()) },test).read({
+        var test = function(obj){return onlineUsers.toString().indexOf(obj) != -1;}
+        userTable.where(test(this.id)).read({
             success : function(users){
                 response.send(statusCodes.OK, { message : users });
             }            
