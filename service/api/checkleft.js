bea399c8798ed19fca73b36959768454d5ce5dd9
windowsazure <windowsazure>
2016-08-12 13:17:19 +0000

    Committing update from request /api/scmvfs/service/api/checkleft.js

--- service/api/checkleft.js
+++ service/api/checkleft.js
@@ -23,7 +23,7 @@ exports.post = function(request, response) {
         if (r.length>0)
         {
             
-           response.send(statusCodes.OK, { message : "User has left", left: true, time: r.createdAt, myTime: dd }); 
+           response.send(statusCodes.OK, { message : "User has left", left: true, time: r[0].__createdAt, myTime: dd }); 
         }
         else
         {
