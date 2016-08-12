bd4945e72b6029c1653e5e04c04658fbed21a4a1
windowsazure <windowsazure>
2016-08-12 12:10:06 +0000

    Committing update from request /api/scmvfs/service/api/checkleft.js

--- service/api/checkleft.js
+++ service/api/checkleft.js
@@ -7,5 +7,5 @@ exports.post = function(request, response) {
 };
 
 exports.get = function(request, response) {
-    response.send(statusCodes.OK, { message : request.params });
+    response.send(statusCodes.OK, { message : request.params.id });
 };
