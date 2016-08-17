3f897e0b8813d1b6d15a16cb71bd8bff9e5f0ed7
windowsazure <windowsazure>
2016-08-17 13:47:04 +0000

    Committing update from request /api/scmvfs/service/api/checkleft.js

--- service/api/checkleft.js
+++ service/api/checkleft.js
@@ -13,7 +13,7 @@ exports.post = function(request, response) {
                    success : function(user){
                        // 2nd phase
                        var currentSecond = (new Date()).getTime();
-                       leftRoomTable.where(function(d_id,u_id,cs,tt){return this.user_id ==  u_id && this.dialog_id == d_id && (tt - cs) > 5000},dialog_id,user_id,parseFloat(currentSecond),parseFloat(this.timecreated)).read({
+                       leftRoomTable.where(function(d_id,u_id,cs,tt){return this.user_id !=  u_id && this.dialog_id == d_id && (tt - cs) > 5000},dialog_id,user_id,parseFloat(currentSecond),parseFloat(this.timecreated)).read({
                            success : function(users){
                                console.log("users : ",users);
                                if(users.length > 0){
@@ -37,7 +37,7 @@ exports.post = function(request, response) {
                         // 2nd phase
                         leftRoomTable.where(function(d_id,u_id,cs,tc)
                         {
-                            return this.user_id ==  u_id && this.dialog_id == d_id && (tc - cs) > 5000
+                            return this.user_id !=  u_id && this.dialog_id == d_id && (tc - cs) > 5000
                         }
                         ,dialog_id,user_id,parseFloat(currentSecond),parseFloat(this.timecreated)
                         ).read({
