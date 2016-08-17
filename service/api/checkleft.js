f2d72b418ee7710eecf7fc5f48a53454576c6ddc
windowsazure <windowsazure>
2016-08-17 13:20:08 +0000

    Committing update from request /api/scmvfs/service/api/checkleft.js

--- service/api/checkleft.js
+++ service/api/checkleft.js
@@ -8,17 +8,13 @@ exports.post = function(request, response) {
         success : function(users){
             if(users.length > 0){
                 // Update
-                console.log("yep 1");
                 users[0].timecreated = (new Date()).getTime();
                 leftRoomTable.update(users[0],{
                    success : function(user){
                        // 2nd phase
-                       console.log("yep 2 ", user);
                        var currentSecond = (new Date()).getTime();
-                       console.log("time ",currentSecond );
                        leftRoomTable.where(function(d_id,u_id,cs){return this.user_id ==  u_id && this.dialog_id == d_id && (this.timecreated- cs) > 5},dialog_id,user_id,currentSecond).read({
                            success : function(users){
-                               console.log("yep 3");
                                if(users.length > 0){
                                    // online
                                    response.send(statusCodes.OK, { boolean : true });
@@ -34,13 +30,12 @@ exports.post = function(request, response) {
             }
             else {
                 // Create
-                var CurrentDate = new Date();
-                leftRoomTable.insert({user_id : user_id, timecreated : CurrentDate},{
+                var currentSecond = (new Date()).getTime();
+                leftRoomTable.insert({user_id : user_id, timecreated : currentSecond},{
                     success : function(user){
                         // 2nd phase
-                       var currentSecond = (new Date()).getSeconds();
-                       leftRoomTable.where(function(d_id,u_id){return this.user_id ==  u_id && this.dialog_id == d_id && ((new Date(this.timecreated.getSeconds())).getSeconds() - currentSecond) > 5},dialog_id,user_id).read({
-                           success : function(users){
+                        leftRoomTable.where(function(d_id,u_id,cs){return this.user_id ==  u_id && this.dialog_id == d_id && (this.timecreated- cs) > 5},dialog_id,user_id,currentSecond).read({
+                                  success : function(users){
                                if(users.length > 0){
                                    // online
                                    response.send(statusCodes.OK, { boolean : true });
