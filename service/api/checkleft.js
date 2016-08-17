9b22748f5af08e00b3776a14da5321dd8208a30e
windowsazure <windowsazure>
2016-08-17 12:55:29 +0000

    Committing update from request /api/scmvfs/service/api/checkleft.js

--- service/api/checkleft.js
+++ service/api/checkleft.js
@@ -9,12 +9,13 @@ exports.post = function(request, response) {
             if(users.length > 0){
                 // Update
                 console.log("yep 1");
+                users[0].timecreated = new Date();
                 leftRoomTable.update(users[0],{
                    success : function(user){
                        // 2nd phase
                        console.log("yep 2 ", user);
                        var currentSecond = (new Date()).getSeconds();
-                       leftRoomTable.where(function(d_id,u_id){return this.user_id ==  u_id && this.dialog_id == d_id && ((new Date(this.__updatedAt.getSeconds())).getSeconds() - currentSecond) > 5},dialog_id,user_id).read({
+                       leftRoomTable.where(function(d_id,u_id){return this.user_id ==  u_id && this.dialog_id == d_id && ((new Date(this.timecreated.getSeconds())).getSeconds() - currentSecond) > 5},dialog_id,user_id).read({
                            success : function(users){
                                console.log("yep 3");
                                if(users.length > 0){
@@ -32,11 +33,12 @@ exports.post = function(request, response) {
             }
             else {
                 // Create
-                leftRoomTable.insert({user_id : user_id},{
+                var CurrentDate = new Date();
+                leftRoomTable.insert({user_id : user_id, timecreated : CurrentDate},{
                     success : function(user){
                         // 2nd phase
                        var currentSecond = (new Date()).getSeconds();
-                       leftRoomTable.where(function(d_id,u_id){return this.user_id ==  u_id && this.dialog_id == d_id && ((new Date(this.__createdAt.getSeconds())).getSeconds() - currentSecond) > 5},dialog_id,user_id).read({
+                       leftRoomTable.where(function(d_id,u_id){return this.user_id ==  u_id && this.dialog_id == d_id && ((new Date(this.timecreated.getSeconds())).getSeconds() - currentSecond) > 5},dialog_id,user_id).read({
                            success : function(users){
                                if(users.length > 0){
                                    // online
