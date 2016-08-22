0805f5fb746f5be7abffaa8c2e6cbaabe05dab6b
windowsazure <windowsazure>
2016-08-22 09:59:41 +0000

    Committing update from request /api/scmvfs/service/api/report_user.js

--- service/api/report_user.js
+++ service/api/report_user.js
@@ -43,13 +43,13 @@ exports.post = function(request, response) {
                             });
                         }
                         else {
-                            response.send(statusCodes.OK, { result : "1. No such user exists.", status : 0});
+                            response.send(statusCodes.OK, { result : "No such user exists.", status : 0});
                         }
                     }
                 });
             }
             else {
-                response.send(statusCodes.OK, { result : "2. Report already exists.", status : 0});
+                response.send(statusCodes.OK, { result : "Report already exists.", status : 0});
             }
         }
     });
