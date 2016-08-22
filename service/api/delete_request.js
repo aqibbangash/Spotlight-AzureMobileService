e573f829c828e7bd5628f18623b6fbca869e69a7
windowsazure <windowsazure>
2016-08-22 08:55:09 +0000

    Committing update from request /api/scmvfs/service/api/delete_request.js

--- service/api/delete_request.js
+++ service/api/delete_request.js
@@ -4,16 +4,22 @@ exports.post = function(request, response) {
     // Tables
     var requestTable    = request.service.tables.getTable('Request');  
     // Get all record with id equals userId  
-    requestTable.where({id : userId,__deleted : false}).read({
+    requestTable.where({id : userId}).read({
         // Set _delete column to true
         success: function(results) {
             if (results.length > 0) {
-                results[0].__deleted=true;
-                requestTable.update(results[0],{
+                requestTable.delete(results,{
                     success:function(res){
-                        response.send(statusCodes.OK, { result : 'User request deleted.', status : 1});
-                    } 
-                });
+                        response.send(statusCodes.OK, { result : 'User request deleted.', status : 1, number:results.length});
+                    }
+                }
+                )
+//                results[0].__deleted=true;
+//                requestTable.update(results[0],{
+//                    success:function(res){
+//                        response.send(statusCodes.OK, { result : 'User request deleted.', status : 1});
+//                    } 
+//                });
             } 
             else {
                   response.send(statusCodes.OK, { result : "No requests for this user.", status : 0});
