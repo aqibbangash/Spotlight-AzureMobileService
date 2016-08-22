6220c79ddbe15a96f77bec3667c6abbd5125070d
windowsazure <windowsazure>
2016-08-22 09:41:42 +0000

    Committing update from request /api/scmvfs/service/api/report_user.js

--- service/api/report_user.js
+++ service/api/report_user.js
@@ -17,7 +17,7 @@ exports.post = function(request, response) {
                 userTable.where({id : userId}).read({
                     success : function(users){
                         if(users.count > 0){
-                            users[0].points += 1;
+                            users[0].points += points;
                             userTable.update(users[0],{
                                 // Insert new report
                                 success : function(user){
