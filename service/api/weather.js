867f822beef2e684d63c879994442448b9ee0798
Aqib Bangash <xs_denied@live.com>
2016-08-23 17:58:53 +0500

    weaher added -2

--- service/api/weather.js
+++ service/api/weather.js
@@ -1,5 +1,6 @@
-var YQL = require('yql');
 exports.get = function(request, response) {
+
+var YQL = require('yql');
       var query = "SELECT * from weather.forecast WHERE (location = @zip)";
       new YQL.exec(query, function(data) {
         console.log("yess","yess");
