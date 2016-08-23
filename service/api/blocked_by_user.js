f849c4319a8e13671f90f0706205359a8a8a9433
windowsazure <windowsazure>
2016-08-23 07:53:27 +0000

    Committing update from request /api/scmvfs/service/api/blocked_by_user.js

--- service/api/blocked_by_user.js
+++ service/api/blocked_by_user.js
@@ -6,16 +6,16 @@ exports.post = function(request, response) {
     // Local variables
     var result    = "";
     
-    var getFlag = function(str){return str.search(userId);}
+    var getFlag = function(str){return str.indexOf(userId);}
     // Get block user by Id
     blockTable.where(function(u){return this.both.indexOf(u)!== -1;},userId).read({
         // add Id present in object
         success : function(res){
             console.log("result 1 : ",res);
-            var flag = getFlag(res.both);
+            var flag = getFlag(res[0].both);
             console.log("flag : ",flag);
             if(flag!=-1 && flag){
-                result+=res.both;
+                result+=res[0].both;
             }
             console.log("result 2: ",result);
             response.send(statusCodes.OK, { returning : result});
